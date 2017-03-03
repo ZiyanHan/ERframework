a8503427bdda36c4f5d55f1db4fb9f89af8b4ae3
gpapadis <gpapadis@yahoo.gr>
2017-03-03 06:19:29 +0200

    Added new

---
+++ src/NewApproaches/ExportDatasets.java
@@ -0,0 +1,72 @@
+package NewApproaches;
+
+import BlockBuilding.StandardBlocking;
+import DataModel.AbstractBlock;
+import DataModel.BilateralBlock;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.util.List;
+
+/**
+ *
+ * @author G.A.P. II
+ */
+public class ExportDatasets {
+
+    private final static String SEGMENT_DELIMITER = ";";
+    private final static String VALUE_DELIMITER = "#";
+    
+    public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException, IOException {
+        String mainDirectory = "/home/gpapadakis/newData/";
+        String[] datasetsPaths = { mainDirectory + "restaurant/",
+                                   mainDirectory + "rexa_dblp/",
+                                   mainDirectory + "imdb_yago/",
+                                   mainDirectory + "bbcMusic/"
+        };
+        
+        String[] d1Datasets = { "restaurant1Profiles ",
+            "rexaProfiles",
+            "yagoProfiles",
+            "bbc-musicNewNoRdfProfiles"
+        };
+
+        String[] d2Datasets = { "restaurant2Profiles",
+            "swetodblp_april_2008Profiles",
+            "imdbProfiles",
+            "dbpedia37NewNoSameAsNoWikipediaProfiles"
+        };
+
+        for (int datasetIndex = 0; datasetIndex < d1Datasets.length; datasetIndex++) {
+            Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(mainDirectory+datasetsPaths[datasetIndex]+"blocks.txt"), "UTF-8"));
+            
+            Preprocessing preprocessing = new Preprocessing(mainDirectory+datasetsPaths[datasetIndex]+d1Datasets[datasetIndex], 
+                    mainDirectory+datasetsPaths[datasetIndex]+d2Datasets[datasetIndex]);
+            
+            final List<AbstractBlock> valueBlocks = preprocessing.getPurgedBlocks(new StandardBlocking());
+            int index = 0;
+            for (AbstractBlock block : valueBlocks) {
+                index++;
+                out.write(index + "\t");
+                
+                BilateralBlock bBlock = (BilateralBlock) block;
+                for (int entityId : bBlock.getIndex1Entities()) {
+                    out.write(entityId + VALUE_DELIMITER);
+                }
+                
+                System.out.print(SEGMENT_DELIMITER);
+                for (int entityId : bBlock.getIndex2Entities()) {
+                    int newId = -entityId;
+                    out.write(newId + VALUE_DELIMITER);
+                }
+                out.write("\n");
+            }
+            
+            out.close();
+        }
+    }
+}
